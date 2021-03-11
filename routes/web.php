e907a66847c625bdc89fc426143670b5a9e033e3
zhenyu2017 <5600145@qq.com>
2021-03-11 18:23:05 +0000

    注册与登录

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'PagesController@root')->name('root');
+Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
