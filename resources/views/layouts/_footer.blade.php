e1ce76585fe0053bcddda2c6fd697b5f7d96b214
zhenyu2017 <5600145@qq.com>
2021-03-11 07:51:40 +0000

    布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+    <p class="float-left"><a href="#" >联系方式：123456789</a></p>
+        
+    <p class="float-right"><a href="mailto:87337365@qq.com">发送邮件</p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title','首页')
+
+@section('content')
+    <H1>这里是首页</H1>
+    
+@stop
