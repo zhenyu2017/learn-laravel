4cf17b73558125253441cc698d8e3030ac712c36
zhenyu2017 <5600145@qq.com>
2021-03-15 14:04:31 +0000

    用户 商吕排序，筛选

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,6 +8,32 @@
     <div class="col-lg-10 offset-lg-1">
         <div class="card">
             <div class="card-body">
+                <form class="search-form" action="{{ route('products.index') }}">
+                    <div class="form-row">
+                        <div class="col-md-9">
+                            <div class="form-row">
+                                <div class="col-auto">
+                                    <input type="text" class="form-control form-control-sm" name="search" placeholder="搜索">
+
+                                </div>
+                                <div class="col-auto">
+                                    <button class="btn btn-primary btn-sm">搜索</button> 
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <select name="order" class="form-control form-control-sm float-right">
+                                <option value="">排序方式</option>
+                                <option value="price_asc">价格从低到高</option>
+                                <option value="price_desc">价格从高到低</option>
+                                <option value="sold_count_asc">销量从低到高</option>
+                                <option value="sold_count_desc">销量从高到低</option>
+                                <option value="rating_asc">评价从低到高</option>
+                                <option value="rating_desc">评价从高到低</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
                 <div class="row products-list">
                     @foreach ($products as $product)
                     <div class="col-3 product-item">
@@ -26,9 +52,23 @@
                     </div>
                     @endforeach
                 </div>
-                <div class="float-right">{{ $products->render() }}</div>
+                <div class="float-right">{{ $products->appends($filters)->render() }}</div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('scriptsAfterJs')
+    <script>
+        var filters = {!! json_encode($filters) !!};
+        $(document).ready(function(){
+            $('.search-form input[name=search]').val(filters.search);
+            $('.search-form select[name=order]').val(filters.order);
+
+            $(".search-form select[name=order]").on('change', function(){
+                $('.search-form').submit();
+            });
+        })
+    </script>
 @endsection
