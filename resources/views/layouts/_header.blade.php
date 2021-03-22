3b3ddab316d6919e661407ec903667a2699104da
zhenyu2017 <5600145@qq.com>
2021-03-22 08:12:22 +0000

    用户 我的订单

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -29,6 +29,7 @@
                         {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('orders.index') }}">我的订单</a>
                         <a class="dropdown-item" href="{{ route('user_addresses.index') }}">收货地址</a>
                         <a class="dropdown-item" href="{{ route('products.favorites') }}">我的收藏</a>
                         <a class="dropdown-item" id="logout" href="#"
