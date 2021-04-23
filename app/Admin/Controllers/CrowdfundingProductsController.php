5af994ad6109ba00dbec0821ea9919d8b80692ed
zhenyu2017 <5600145@qq.com>
2021-04-23 07:58:32 +0000

    后台 众筹商品管理

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -3,20 +3,21 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Category;
+use App\Models\CrowdfundingProduct;
 use App\Models\Product;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class ProductsController extends AdminController
+class CrowdfundingProductsController extends AdminController
 {
     /**
      * Title for current resource.
      *
      * @var string
      */
-    protected $title = '商品';
+    protected $title = '众筹商品';
 
     /**
      * Make a grid builder.
@@ -26,20 +27,20 @@ class ProductsController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Product());
-        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
-        
-        
+
+        $grid->model()->where('type', Product::TYPE_CROWDFUNDING);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
-        $grid->column('category.name', '类目');
         $grid->on_sale('已上架')->display(function($value){
-            return $value ? '是' : '否';
+            return $value ? '是':'否';
         });
         $grid->price('价格');
-        $grid->rating('评分');
-        $grid->sold_count('销量');
-        $grid->review_count('评论');
-
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', '状态')->display(function($value){
+            return CrowdfundingProduct::$statusMap[$value];
+        });
         $grid->actions(function($actions){
             $actions->disableView();
             $actions->disableDelete();
@@ -50,7 +51,6 @@ class ProductsController extends AdminController
                 $batch->disableDelete();
             });
         });
-        
 
         return $grid;
     }
@@ -66,26 +66,28 @@ class ProductsController extends AdminController
     {
         $form = new Form(new Product());
 
-        $form->hidden('type')->default(Product::TYPE_NORMAL);
+        $form->hidden('type')->default(Product::TYPE_CROWDFUNDING);
         $form->text('title', '商品名称')->rules('required');
         $form->select('category_id', '类目')->options(function($id){
             $category = Category::find($id);
             if ($category) {
                 return [$category->id => $category->full_name];
             }
-        })->ajax('/admin/api/categories?is_directory=0');
+        })->ajax('/admin/api/categories?is_directroy=0');
         $form->image('image', '封面图片')->rules('required|image');
         $form->quill('description', '商品描述')->rules('required');
         $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-        $form->hasMany('skus', 'SKU 列表', function(Form\NestedForm $form){
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+        $form->hasMany('skus', '商品SKU', function(Form\NestedForm $form){
             $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'sku 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|integer|min:0.01');
-            $form->text('stock', '库存')->rules('required|integer|min:0');
-        });
+            $form->text('description', 'SKU 描述')->rules('required');
+            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
 
-        $form->saving(function(Form $form){
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
+        $form->saving(function (Form $form) {
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
         });
 
 
